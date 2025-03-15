8fae9752cef6f7a5d8e0c2d7e08e08292f6d055e
tamara_trych1 <tamara_trych1@epam.com>
2025-03-15 17:11:01 +0300

    HW_5 Tamara_Trych

--- Python_hw5_Tamara_Trych.py
+++ Python_hw5_Tamara_Trych.py
@@ -16,109 +16,111 @@ You need to implement:
 Each new record should be added to the end of file. Commit file in git for review.
 """
 from datetime import datetime, date
-from abc import ABC, abstractmethod
-import pandas as pd
 
 class Publication:
-    def __init__(self, pulication_text, publication_date):
+    def __init__(self, pulication_text):
         self.pulication_text = pulication_text
-        self.publication_date = publication_date
+        self.publication_date = datetime.now()
+        self.title = ''
+        self.spesial_info = ''
 
-    @abstractmethod
     def publish(self):
-        pass
+        try:
+            with open("publication.txt", "a", encoding="utf-8") as f:
+                f.write(self.title + '\n')
+                f.write(self.pulication_text + '\n')
+                f.write(self.spesial_info + '\n\n')
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 class News(Publication):
-    def __init__(self, pulication_text,  publication_city, publication_date=datetime.now()):
-        Publication.__init__(self, pulication_text, publication_date)
-        self.city = publication_city
+    def __init__(self, pulication_text,  publication_city):
+        super().__init__(pulication_text)
         self.publication_date_formated = self.publication_date.strftime("%m/%d/%Y %H:%M")
+        self.title = "News -------------------------"
+        self.spesial_info = f"{publication_city},  {self.publication_date_formated}"
 
-    def publish(self):
-        with open("publication.txt", "a") as f:
-            f.write("News -------------------------\n")
-            f.write(self.pulication_text + '\n')
-            f.write(f"{self.city},  {self.publication_date_formated}\n\n")
+    @classmethod
+    def create_from_input(cls):
+        text = input('Type a text of your news\n')
+        city = input('Type a text of a city\n')
+        return cls(text, city)
 
 
 class Ad(Publication):
-    def __init__(self, pulication_text, expiration_date, publication_date=datetime.now()):
-        Publication.__init__(self, pulication_text, publication_date)
+    def __init__(self, pulication_text, expiration_date):
+        super().__init__(pulication_text)
         self.expiration_date = expiration_date
         self.expiration_date_formated = self.expiration_date.strftime("%m/%d/%Y")
         self.days_left = self.expiration_date - self.publication_date.date()
+        self.title = "Private Ad -------------------"
+        self.spesial_info = f"Actual until {self.expiration_date_formated}, { self.days_left.days} days left"
 
-
-    def publish(self):
-        with open("publication.txt", "a") as f:
-            f.write("Private Ad -------------------\n")
-            f.write(self.pulication_text + '\n')
-            f.write(f"Actual until {self.expiration_date_formated}, {pd.Timedelta(self.days_left).days} days left\n\n")
+    @classmethod
+    def create_from_input(cls):
+        text = input('Type a text of your advertisement\n')
+        while True:
+            day = input(
+                'Type expiration date of your advertisement. At first, day of the month (type a figure from 1 to 31)\n')
+            month = input('Second, the month (type a figure from 1 to 12)\n')
+            year = input('Finally, year (type four figures)\n')
+            try:
+                expiration_date = date(int(year), int(month), int(day))
+                if expiration_date < date.today():
+                    print("The date should be set in the future. Try again.")
+                else:
+                    print(f"Expiration date is set to: {expiration_date}")
+                    break
+            except:
+                print("You entered a wrong date. Try again.")
+        return cls(text, expiration_date)
 
 
 class RentOfDay(Publication):
     def __init__(self, address, price, square):
-        self.pulication_text = f"Rent the flat of {square} m2 in {address} for {price} USD"
-        self.rent_rate = 'best price' if int(price) < 500 else 'best condition'
+        pulication_text = f"Rent the flat of {square} m2 in {address} for {price} USD"
+        super().__init__(pulication_text)
+        self.title = "Rent of the day --------------"
+        self.spesial_info = 'best price' if int(price) < 500 else 'best condition'
 
-    def publish(self):
-        with open("publication.txt", "a") as f:
-            f.write("Rent of the day --------------\n")
-            f.write(self.pulication_text + '\n')
-            f.write(self.rent_rate + '\n\n')
+    @classmethod
+    def create_from_input(cls):
+        address = input('Type address of a flat for rent:\n')
+        while True:
+            price = input('Type price (number) per month in dollars of a flat for rent:\n')
+            try:
+                price = int(price)
+                break
+            except:
+                print("You entered a wrong price. Type only numbers.")
+        while True:
+            square = input('Type square (number) of a flat for rent:\n')
+            try:
+                square = int(square)
+                break
+            except:
+                print("You entered a wrong square. Type only numbers.")
+        return cls(address, price, square)
 
 
 class Writer:
-    counter = 5
-    next_publication = ""
-    while counter != "0":
-        counter = input('Choose a type of publication:\n 1. To choose news type "1" and press Enter\n 2. To choose advertisement type "2" and press Enter\n 3. To choose RentOfDay type "3" and press Enter\n type another symbol and press Enter to Exit\n')
-        if counter not in ("1", "2", "3"):
+    def __init__(self):
+        self.run()
+
+    while True:
+        counter = input(
+            'Choose a type of publication:\n 1. To choose news type "1" and press Enter\n 2. To choose advertisement type "2" and press Enter\n 3. To choose RentOfDay type "3" and press Enter\n Type another symbol and press Enter to Exit\n')
+        if counter == "1":
+            publication = News.create_from_input()
+        elif counter == "2":
+            publication = Ad.create_from_input()
+        elif counter == "3":
+            publication = RentOfDay.create_from_input()
+        else:
             print("You decide to finish publishing")
             break
-        elif counter == "1":
-            text = input('Type a text of your news\n')
-            city = input('Type a text of a city\n')
-            next_publication = News(text, city)
-        elif counter == "2":
-            text = input('Type a text of your advertisement\n')
-            expiration_date = ""
-            while True:
-                day = input('Type expiration date of your advertisement. At first, day of the month (type a figure from 1 to 31)\n')
-                month = input('Second, the month (type a figure from 1 to 12)\n')
-                year = input('Finally, year (type four figures)\n')
-                try:
-                    expiration_date = date(int(year), int(month), int(day))
-                    if expiration_date < date.today():
-                        print("The date should be set in the future. Try again.")
-                    else:
-                        print(f"Expiration date is set to: {expiration_date}")
-                        break
-                except:
-                    print("You enter wrong date. Try again.")
-            next_publication = Ad(text, expiration_date)
-        elif counter == "3":
-            address = input('Type address of a flat for rent:\n')
-            while True:
-                price = input('Type price (number) per month in dollars of a flat for rent:\n')
-                try:
-                    int(price)
-                    break
-                except:
-                    print("You enter wrong price. Type only number")
-            while True:
-                square = input('Type square (number) of a flat for rent:\n')
-                try:
-                    int(square)
-                    break
-                except:
-                    print("You enter wrong square. Type only number")
-            next_publication = RentOfDay(address, price, square)
-        else:
-            print("Please, make your choice")
-        next_publication.publish()
-
+        publication.publish()
 
 if __name__ == "__main__":
     Writer()
