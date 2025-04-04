1f3fcbe90e534d54b1719f184de1bb3d1c3939b1
tamara_trych1 <tamara_trych1@epam.com>
2025-04-05 02:11:31 +0400

    Final task Tamara_Trych

--- FinalTask/Modules/Distance.py
+++ FinalTask/Modules/Distance.py
@@ -1,15 +1,25 @@
-from geopy.distance import geodesic
+import math
+
+EarthRadius = 6371.0
 
 
 class Distance:
     def __init__(self, city_from, city_to, latitude_from, longitude_from, latitude_to, longitude_to):
         self.city_from = city_from
         self.city_to = city_to
-        self.point_from = (latitude_from, longitude_from)
-        self.point_to = (latitude_to, longitude_to)
+        self.latitude_from = math.radians(latitude_from)
+        self.longitude_from = math.radians(longitude_from)
+        self.latitude_to = math.radians(latitude_to)
+        self.longitude_to = math.radians(longitude_to)
 
     def calculate_distance(self):
-        distance = geodesic(self.point_from, self.point_to).kilometers
+        dlat = self.latitude_to - self.latitude_from
+        dlon = self.longitude_to - self.longitude_from
+
+        a = math.sin(dlat / 2) ** 2 + math.cos(self.latitude_to) * math.cos(self.latitude_from) * math.sin(dlon / 2) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = EarthRadius * c
+
         return distance
 
 
